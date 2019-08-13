4b0dc1d3b756b9be9942be723912e2977b0079d7
broeschi <39580893+broeschi@users.noreply.github.com>
2019-08-13 15:07:55 +0200

    StepDefs_DataDrivenSteps

--- New_Cucumber/resources/features/DataDrivenTests.feature
+++ New_Cucumber/resources/features/DataDrivenTests.feature
@@ -2,6 +2,6 @@ Feature: Data Driven Tests
 This feature covers different examples of data driven testing in Cucumber
 
 Scenario: Perform searsch operation on Google Homepage
-Given I Launch Chrome browser
-When I open Google Homeage
+Given I launch Chrome browser
+When I open Google Homepage
 Then I search for "weather tommorow" on Google
---
+++ New_Cucumber/src/stepDefinitions/StepDefs_DataDrivenSteps.java
@@ -0,0 +1,12 @@
+package stepDefinitions;
+
+import cucumber.api.java.en.Then;
+
+public class StepDefs_DataDrivenSteps {
+	@Then("^I search for {string} on Google$")
+	public void i_search_for_on_Google(String searchKeyword) throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    // throw new cucumber.api.PendingException();
+	}
+
+}
