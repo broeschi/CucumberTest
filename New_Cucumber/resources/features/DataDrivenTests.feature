4b5762579543539c98460ac29c7985c7c78307bb
broeschi <39580893+broeschi@users.noreply.github.com>
2019-08-16 15:07:13 +0200

    Bis hierher funktionierts, muss nur noch verstehen, warum.

--- New_Cucumber/resources/features/DataDrivenTests.feature
+++ New_Cucumber/resources/features/DataDrivenTests.feature
@@ -1,7 +1,7 @@
 Feature: Data Driven Tests
 This feature covers different examples of data driven testing in Cucumber
 
-Scenario: Perform searsch operation on Google Homepage
+Scenario: Perform search operation on Google Homepage
 Given I launch Chrome browser
 When I open Google Homepage
 Then I search for "weather tomorow" on Google
--- New_Cucumber/src/testRunners/TestRunner_GoogleHomepage.java
+++ New_Cucumber/src/testRunners/TestRunner_GoogleHomepage.java
@@ -5,7 +5,11 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="resources/features", glue="stepDefinitions")
+@CucumberOptions(
+	features = "resources/features/GoogleHomepage.feature", 
+	glue = "stepDefinitions"
+	)
+	
 public class TestRunner_GoogleHomepage {
 
 }
